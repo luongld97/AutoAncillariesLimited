69149a232d7e88b7413b6caa50d59c875acef5bc
Lưu Đại Lương <luongld.it@gmail.com>
2017-06-19 20:15:37 +0700

    Tạo project ( project zero )

---
+++ AutoAncillariesLimited/AutoAncillariesLimited/Models/CategoryMetaData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AutoAncillariesLimited.Models
+{
+  public class CategoryMetaData
+  {
+    [Required]
+    [MinLength(6)]
+    [MaxLength(100)]
+    public string Name { get; set; }
+  }
+
+  [MetadataType(typeof(CategoryMetaData))]
+  public partial class Category
+  {
+    
+  }
+}
